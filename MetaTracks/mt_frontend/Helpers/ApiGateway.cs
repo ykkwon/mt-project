dc375d4f61e489dee199d1d4be92369706b58a54
nekris <kristoffer.ln@gmail.com>
2016-01-28 23:12:51 +0100

    Added helper class for calling the Web API.
    Added ApiGateway.cs. Added a stylesheet.

---
+++ MetaTracks/mt_frontend/Helpers/ApiGateway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mt_frontend.Helpers
+{
+    public class ApiGateway
+    {
+        // Call the WebAPI 
+    }
+}
