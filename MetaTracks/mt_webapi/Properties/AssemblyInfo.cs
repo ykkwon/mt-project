72535a458c97620e6b9d5d3cd13ffb88fc73fa1e
nekris <kristoffer.ln@gmail.com>
2016-01-28 21:44:58 +0100

    Decoupled project.
    Added separate WebAPI, SPA and common libraries project.

--- MetaTracks/mt_webapi/Properties/AssemblyInfo.cs
+++ MetaTracks/mt_webapi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MTWorking")]
+[assembly: AssemblyTitle("mt_webapi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MTWorking")]
+[assembly: AssemblyProduct("mt_webapi")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("556236e8-b376-4a34-a76b-21dd35fe162b")]
+[assembly: Guid("929a6081-9fdc-415a-b1ce-dbc0f027f067")]
 
 // Version information for an assembly consists of the following four values:
 //
