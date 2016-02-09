28f444f6759f6111d768640a27ae84f953a3645b
nekris <kristoffer.ln@gmail.com>
2016-02-09 11:58:37 +0100

    MP4 to WAV conversion is now working.

--- MetaTracks/mt_commonlib/Fingerprint.cs
+++ MetaTracks/mt_commonlib/Fingerprint.cs
@@ -1,151 +1,15 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-using NAudio.MediaFoundation;
-using NAudio.Wave;
+﻿using NAudio.Wave;
 
 namespace mt_commonlib
-// TODO: Receive .mp4 file (or possibly other formats) from dbapp.
-// TODO: Execute fingerprinting algorithm (Algorithm.cs)
-// TODO: Send hash directly to back-end
-
 {
-    class Fingerprint : IFingerprint
+    class Fingerprint
     {
-        private static Movie testMovie = new Movie(@"..\..\Resources\testFile.mp4");
-        private static String waString; // Debugging 
+        public WaveFormat Wave { get; set; }
 
-        static void Main(string[] args)
+        public Fingerprint(WaveFormat Wave)
         {
-            PlayFile(testMovie);
-            Console.WriteLine(@"Application ended . . .");
-            Console.ReadKey();
-            //Preprocess(testMovie);
-        }
-
-        public void ReceiveMovie(Object file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Receive(object file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReceiveFingerprint(Object file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Receive(string file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReceiveFingerprint(string file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Send(string hash)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IFingerprint.Preprocess(Movie movie)
-        {
-            Preprocess(movie);
-        }
-
-        public void ComputeSpectrogram(object file)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Filter(Array stringArray)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ComputeWavelets(Array stringArray)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void HashTransform(Array waveletArray)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IFingerprint.PlayFile(Movie movie)
-        {
-            PlayFile(movie);
-        }
-
-        private static void PlayFile(Movie movie)
-        {
-            {
-                using (var reader = new MediaFoundationReader(movie.Path))
-                using (var waveout = new WaveOutEvent())
-                {
-                    waveout.Init(reader);
-                    waString = waveout.OutputWaveFormat.ToString();
-                    Preprocess(movie);
-                    waveout.Play();
-                    
-                    while (waveout.PlaybackState == PlaybackState.Playing)
-                    {
-                        Thread.Sleep(1000);
-                    }
-
-                }
-            }
-        }
-
-        public void Send()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Preprocess(Movie movie)
-        {
-            Console.WriteLine(waString);
-        }
-
-        public void ComputeSpectrogram()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Filter()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ComputeWavelets()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void HashTransform()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Receive(Movie movie)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReceiveFingerprint(Movie movie)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ComputeSpectrogram(Movie movie)
-        {
-            throw new NotImplementedException();
+            this.Wave = Wave;
         }
     }
 }
+
---
+++ MetaTracks/mt_commonlib/FingerprintManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using NAudio.Wave;
+
+namespace mt_commonlib
+{
+    class FingerprintManager
+    {
+        public FingerprintManager() { }
+
+        static void Main(string[] args)
+        {
+            FingerprintManager fpm = new FingerprintManager();
+            InputVideo testVideo = new InputVideo(@"..\..\Resources\testFile.mp4");
+            fpm.Mp4ToWav(testVideo, "wavOutput.wav");
+            using (var reader = new MediaFoundationReader(testVideo.FilePath))
+            using (var waveout = new WaveOutEvent())
+            {
+                waveout.Init(reader);
+                waveout.Play();
+
+
+                while (waveout.PlaybackState == PlaybackState.Playing)
+                {
+                    Console.WriteLine(waveout.OutputWaveFormat);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        
+    
+
+        public void ReceiveMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveFingerprint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendToDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Preprocess()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ComputeSpectrogram()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Filter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ComputeWavelets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HashTransform()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Mp4ToWav(InputVideo video, string outputFile)
+        {
+            using (MediaFoundationReader reader = new MediaFoundationReader(video.FilePath))
+            {
+                using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                {
+                    WaveFileWriter.CreateWaveFile(outputFile, pcmStream);
+                }
+            }
+        }
+    }
+}
