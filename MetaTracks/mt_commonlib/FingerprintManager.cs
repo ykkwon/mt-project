3326293c76a44a25c1bb15b12f2ec9744c632ea7
nekris <kristoffer.ln@gmail.com>
2016-02-10 09:23:42 +0100

    Refactoring awaiting the upcoming merge

--- MetaTracks/mt_commonlib/FingerprintManager.cs
+++ MetaTracks/mt_commonlib/FingerprintManager.cs
@@ -1,53 +1,48 @@
 ﻿using System;
-using System.Resources;
+using System.IO;
 using System.Threading;
 using NAudio.Wave;
 
 namespace mt_commonlib
 {
+    // TODO: Confirm that Mp4toWav actually works.
+    // TODO: Confirm that Preprocess actually works.
+    // TODO: Thread pooled receive.
+    // TODO: Receive.. SOCKETS? 
+    // TODO: Possibly GUI with some event handlers.
+    // TODO: Plot waveline.
+
     class FingerprintManager
     {
-        public FingerprintManager() { }
-
-        static void Main(string[] args)
+        static void Main()
         {
-            try { 
-            System.IO.File.Delete(@"..\..\Resources\wavOutput.wav");
-            Console.WriteLine(@"Old wav output was trunctuated.");
-            System.IO.File.Delete(@"..\..\Resources\preprocessedOutput.wav");
-            Console.WriteLine(@"Old preprocessed output was trunctuated.");
-            }
-            catch (System.IO.IOException e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-            int desiredFrequency = 5512;
-            int desiredChannels = 1; // Mono
-            FingerprintManager fpm = new FingerprintManager();
-            Video testVideo = new Video(@"..\..\Resources\testTrailer.mp4");
-            fpm.Mp4ToWav(testVideo, @"..\..\Resources\wavOutput.wav");
-            Video convertedVideo = new Video(@"..\..\Resources\wavOutput.wav");
-            fpm.Preprocess(convertedVideo, @"..\..\Resources\preprocessedOutput.wav", desiredFrequency, desiredChannels);
-            Video preprocessedVideo = new Video(@"..\..\Resources\preprocessedOutput.wav");
-            
+            const string resourcePath = @"..\..\Resources\";
+            const int desiredFrequency = 5512; // 5512 contains all the relevant information
+            const int desiredChannels = 1; // Mono
+            var fpm = new FingerprintManager();
+            var testVideo = new Video(resourcePath + "testTrailer.mp4");
+            fpm.CleanAndTrunctuate();
+            fpm.Mp4ToWav(testVideo, resourcePath + "wavOutput.wav");
+            var convertedVideo = new Video(resourcePath + "wavOutput.wav"); // Video converted to .wav from input multimedia
+            fpm.Preprocess(convertedVideo, resourcePath + "preprocessedOutput.wav", desiredFrequency, desiredChannels); 
+            var preprocessedVideo = new Video(resourcePath + "preprocessedOutput.wav"); // Preprocessed .wav, reduced to 5512Hz mono
+
             using (var reader = new MediaFoundationReader(preprocessedVideo.FilePath))
             using (var waveout = new WaveOutEvent())
             {
                 waveout.Init(reader);
-                Console.WriteLine("Initiating playback.");
+                Console.WriteLine(@"Initiating playback.");
                 waveout.Play();
 
 
                 while (waveout.PlaybackState == PlaybackState.Playing)
                 {
-                    Console.WriteLine("Output: " + waveout.OutputWaveFormat + " Position: " + waveout.GetPosition() + " Bytes written to waveout.");
+                    Console.WriteLine(@"Output: " + waveout.OutputWaveFormat + @" Position: " + waveout.GetPosition() + @" Bytes written to waveout.");
                     Thread.Sleep(5000);
                 }
-                Console.WriteLine("Playback ended.");
+                Console.WriteLine(@"Playback ended.");
             }
         }
-
 
         public void ReceiveMovie()
         {
@@ -73,7 +68,7 @@ namespace mt_commonlib
                 {
                     resampler.ResamplerQuality = 60;
                     WaveFileWriter.CreateWaveFile(outputFile, resampler);
-                    Console.WriteLine("Preprocessing done.");
+                    Console.WriteLine(@"Preprocessing done.");
                 }
             }
         }
@@ -106,7 +101,35 @@ namespace mt_commonlib
                 using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
                 {
                     WaveFileWriter.CreateWaveFile(outputFile, pcmStream);
-                    Console.WriteLine("MP4 to WAV conversion done.");
+                    Console.WriteLine(@"MP4 to WAV conversion done.");
+                }
+            }
+        }
+
+        public void CleanAndTrunctuate()
+        {
+            if (File.Exists(@"..\..\Resources\wavOutput.wav"))
+            {
+                try
+                {
+                    File.Delete(@"..\..\Resources\wavOutput.wav");
+                    Console.WriteLine(@"Old wav output was trunctuated.");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            if (File.Exists(@"..\..\Resources\preprocessedOutput.wav"))
+            {
+                try
+                {
+                    File.Delete(@"..\..\Resources\preprocessedOutput.wav");
+                    Console.WriteLine(@"Old preprocessed output was trunctuated.");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
                 }
             }
         }
