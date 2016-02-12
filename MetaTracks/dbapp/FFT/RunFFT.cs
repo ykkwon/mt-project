c15eecd7b74b9bde1eb5efb965589cab2fa82cfa
Kristian Støylen <kristian.stoylen93@gmail.com>
2016-02-12 11:08:22 +0100

    RunFFT class constructor

--- MetaTracks/dbapp/FFT/RunFFT.cs
+++ MetaTracks/dbapp/FFT/RunFFT.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using NAudio.Wave;
 
 namespace dbApp.FFT
 {
     class RunFFT
     {
+        public RunFFT(string filepath)
+        {
+            try
+            {
+
+                var inputStream = new AudioFileReader(filepath);
+                var aggregator = new SampleAggregator(inputStream);
+
+                aggregator.NotificationCount = inputStream.WaveFormat.SampleRate/100;
+                aggregator.PerformFFT = true;
+                aggregator.FftCalculated += (sender, args) => OnFftCalculated(args);
+                aggregator.MaximumCalculated += (sender, args) => OnMaximumCalculated(args);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Problem has occured in fftrun");
+            }
+        }
+
+        private void OnMaximumCalculated(MaxSampleEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnFftCalculated(FftEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
