735478e959a09d1ea63be804b6871403bf172097
nekris <kristoffer.ln@gmail.com>
2016-02-22 13:38:27 +0100

    Added standard ASP.NET template

---
+++ MetaTracks/FingerprintManagementConsole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FingerprintManagementConsole.MvcApplication" Language="C#" %>
---
+++ MetaTracks/FingerprintManagementConsole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FingerprintManagementConsole.Startup))]
+namespace FingerprintManagementConsole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
