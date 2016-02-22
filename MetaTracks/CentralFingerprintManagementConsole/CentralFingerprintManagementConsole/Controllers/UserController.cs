d7d16a0f61035c2120ebab7264701e14c20e7315
nekris <kristoffer.ln@gmail.com>
2016-02-22 16:13:03 +0100

    Stripped down management console. Login working.

---
+++ MetaTracks/CentralFingerprintManagementConsole/CentralFingerprintManagementConsole/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CentralFingerprintManagementConsole.Controllers
+{
+        public class UserController : Controller
+        {
+            //
+            // GET: /User/
+            public ActionResult Index()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public ActionResult Login(CentralFingerprintManagementConsole.Models.User user)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (user.IsValid(user.UserName, user.Password))
+                    {
+                        FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Login data is incorrect!");
+                    }
+                }
+                return View(user);
+            }
+            public ActionResult Logout()
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
