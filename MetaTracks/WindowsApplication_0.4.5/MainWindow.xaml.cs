03b368946a57c06197a86d6046fef8c312630446
nekris <kristoffer.ln@gmail.com>
2016-03-11 13:10:23 +0100

    Added empty iOS Application
    , some changes to WPF Windows Application

--- MetaTracks/WindowsApplication_0.4.5/MainWindow.xaml.cs
+++ MetaTracks/WindowsApplication_0.4.5/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
 
 namespace WindowsApplication_0._4._5
 {
@@ -35,7 +37,36 @@ namespace WindowsApplication_0._4._5
 
         private void refreshButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            {
+                // Creates a list that holds all in audio devices
+                List<NAudio.Wave.WaveInCapabilities> sources = new List<WaveInCapabilities>();
+
+                // Loops over all audio devices
+                for (int i = 0; i < WaveIn.DeviceCount; i++)
+                {
+                    // populates the list with audio devices
+                    sources.Add(WaveIn.GetCapabilities(i));
+                }
+
+                // Clears the source list to make sure we start from a blank slate
+                sourceList.Items.Clear();
+
+                // Adds each resource to the listView in the form
+                foreach (var source in sources)
+                {
+                    sourceList.Items.Add(new MyItem { Device = source.ProductName, Channels = source.ProductGuid.ToString() });
+                    //ListViewItem item = new ListViewItem(source.ProductName);
+                    //item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                    //sourceList.Items.Add(item);
+                }
+            }
+
+        }
+        public class MyItem
+        {
+            public string Device { get; set; }
+
+            public string Channels { get; set; }
         }
     }
 }
