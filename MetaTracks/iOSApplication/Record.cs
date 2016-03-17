0c23fa45b5d1759e409d0fa25c021407df2996a3
nekris <kristoffer.ln@gmail.com>
2016-03-17 22:31:15 +0100

    Added lambda threads (iOS)

--- MetaTracks/iOSApplication/Record.cs
+++ MetaTracks/iOSApplication/Record.cs
@@ -12,64 +12,79 @@ namespace iOSApplication
 {
     public static class Record
     {
-        static AVAudioRecorder recorder;
-        static NSError error;
-        static NSUrl url;
-        static NSDictionary settings;
+        static AVAudioRecorder _recorder;
+        static NSError _error;
+        static NSUrl _url;
+        static NSDictionary _settings;
 
         public static void StopRecording()
         {
-            recorder.Stop();
+            new Thread(() =>
+            {
+                if (_recorder.Recording)
+                {
+                    _recorder.Stop();
+                }
+                Console.WriteLine("Recorder is not currently running.");
+            }).Start();
         }
 
         public static void PrepareRecording(int fileIterator)
         {
-            var audioSession = AVAudioSession.SharedInstance();
-            audioSession.SetCategory(AVAudioSessionCategory.PlayAndRecord);
-            string fileName = string.Format("Myfile{0}.wav", fileIterator);
-            string audioFilePath = Path.Combine("/Users/metatracks/Desktop", fileName);
-            Console.WriteLine("Audio File Path: " + audioFilePath);
-
-            url = NSUrl.FromFilename(audioFilePath);
-            //set up the NSObject Array of values that will be combined with the keys to make the NSDictionary
-            NSObject[] values =
+            new Thread(() =>
             {
-                NSNumber.FromFloat(5512.0f), //Sample Rate
-                NSNumber.FromInt32((int) AudioToolbox.AudioFormatType.LinearPCM), //AVFormat
-                NSNumber.FromInt32(1), //Channels
-                NSNumber.FromInt32(16), //PCMBitDepth
-                NSNumber.FromBoolean(false), //IsBigEndianKey
-                NSNumber.FromBoolean(false) //IsFloatKey 
-            };
+                var audioSession = AVAudioSession.SharedInstance();
+                audioSession.SetCategory(AVAudioSessionCategory.PlayAndRecord);
+                string fileName = string.Format("Myfile{0}.wav", fileIterator);
+                string audioFilePath = Path.Combine("/Users/metatracks/Desktop", fileName);
+                Console.WriteLine("Audio File Path: " + audioFilePath);
 
-            //Set up the NSObject Array of keys that will be combined with the values to make the NSDictionary
-            NSObject[] keys = {
-                AVAudioSettings.AVSampleRateKey,
-                AVAudioSettings.AVFormatIDKey,
-                AVAudioSettings.AVNumberOfChannelsKey,
-                AVAudioSettings.AVLinearPCMBitDepthKey,
-                AVAudioSettings.AVLinearPCMIsBigEndianKey,
-                AVAudioSettings.AVLinearPCMIsFloatKey
-            };
+                _url = NSUrl.FromFilename(audioFilePath);
+                //set up the NSObject Array of values that will be combined with the keys to make the NSDictionary
+                NSObject[] values =
+                {
+                    NSNumber.FromFloat(5512.0f), //Sample Rate
+                    NSNumber.FromInt32((int) AudioToolbox.AudioFormatType.LinearPCM), //AVFormat
+                    NSNumber.FromInt32(1), //Channels
+                    NSNumber.FromInt32(16), //PCMBitDepth
+                    NSNumber.FromBoolean(false), //IsBigEndianKey
+                    NSNumber.FromBoolean(false) //IsFloatKey 
+                };
 
-            //Set Settings with the Values and Keys to create the NSDictionary
-            settings = NSDictionary.FromObjectsAndKeys(values, keys);
+                //Set up the NSObject Array of keys that will be combined with the values to make the NSDictionary
+                NSObject[] keys =
+                {
+                    AVAudioSettings.AVSampleRateKey,
+                    AVAudioSettings.AVFormatIDKey,
+                    AVAudioSettings.AVNumberOfChannelsKey,
+                    AVAudioSettings.AVLinearPCMBitDepthKey,
+                    AVAudioSettings.AVLinearPCMIsBigEndianKey,
+                    AVAudioSettings.AVLinearPCMIsFloatKey
+                };
 
-            //Set recorder parameters
-            recorder = AVAudioRecorder.Create(url, new AudioSettings(settings), out error);
-            //Set Recorder to Prepare To Record
-            recorder.PrepareToRecord();
-            DoRecord();
+                //Set Settings with the Values and Keys to create the NSDictionary
+                _settings = NSDictionary.FromObjectsAndKeys(values, keys);
+
+                //Set recorder parameters
+                _recorder = AVAudioRecorder.Create(_url, new AudioSettings(_settings), out _error);
+                //Set Recorder to Prepare To Record
+                _recorder.PrepareToRecord();
+                DoRecord();
+            }).Start();
         }
 
-        public static bool DoRecord()
+        public static void DoRecord()
         {
-            // This writes data to the wave file.
-            recorder.Record();
-            Thread.Sleep(1000);
-            recorder.Stop();
-            Console.WriteLine("Finished recording.");
-            return true;
+            (new Thread(() =>
+            {
+                {
+                    // This writes data to the wave file.
+                    _recorder.Record();
+                    Thread.Sleep(1000);
+                    _recorder.Stop();
+                    Console.WriteLine("Finished recording.");
+                }
+            })).Start();
         }
     }
 }
