71dc6f0520a609fb23a72dc0de0c4486ac1e4ca9
nekris <kristoffer.ln@gmail.com>
2016-03-17 14:02:52 +0100

    Added iOS application with event handlers.

---
+++ MetaTracks/iOSApplication/Record.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AcousticFingerprintingLibrary;
+using AcousticFingerprintingLibrary.SoundFingerprint;
+using AcousticFingerprintingLibrary.SoundFingerprint.AudioProxies;
+using AcousticFingerprintingLibrary.SoundFingerprint.AudioProxies.Strides;
+
+namespace iOSApplication
+{
+    public static class Record
+    {
+        public static void WriteToForeground()
+        {
+            FingerprintManager fm = new FingerprintManager();
+            Console.WriteLine("TEST");
+        }
+
+        public static void getInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MetaTracks/iOSApplication/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+
+namespace iOSApplication
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            RecordButton.TouchUpInside += (object sender, EventArgs e) => {
+                Record.WriteToForeground();
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
