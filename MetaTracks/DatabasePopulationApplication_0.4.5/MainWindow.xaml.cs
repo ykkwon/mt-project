d0daa840580686e887ef6941700c7ce2f85f2061
nekris <kristoffer.ln@gmail.com>
2016-03-24 16:16:46 +0100

    Changes to dbApp

--- MetaTracks/DatabasePopulationApplication_0.4.5/MainWindow.xaml.cs
+++ MetaTracks/DatabasePopulationApplication_0.4.5/MainWindow.xaml.cs
@@ -32,10 +32,7 @@ namespace DatabasePopulationApplication_0._4._5
         //FingerprintDatabaseManager fdm = new FingerprintDatabaseManager();
         private string _entryName;
         private string _typeName;
-        private OpenFileDialog _open;
         private string filename;
-        private Media _convertedMedia;
-        private Media _preprocessedMedia;
 
         private void openButton_Click(object sender, RoutedEventArgs e)
         {
@@ -63,21 +60,6 @@ namespace DatabasePopulationApplication_0._4._5
             {
                 try
                 {
-                    /*
-                    _open = new OpenFileDialog
-                    {
-                        Filter = "MP4 Video File (*.mp4)|*.mp4;|AVI Video File (*.avi)|*.avi;|All files (*.*)|(*.*);",
-                    };
-                    _open.ShowDialog();
-
-                    var inputMedia = new Media(_open.FileName);
-                    Console.WriteLine(@"File path: " + inputMedia.FilePath);
-                    Console.WriteLine(@"File name: " + inputMedia.FileName);
-                    Console.WriteLine(@"Converting input to Wave format.");
-                    _convertedMedia = fm.ConvertToWav(inputMedia);
-                    _preprocessedMedia = fm.Preprocess(_convertedMedia, 5512);
-                    */
-
                     OpenFileDialog ofd = new OpenFileDialog();
                     ofd.ShowDialog();
                     filename = ofd.FileName;
@@ -92,74 +74,39 @@ namespace DatabasePopulationApplication_0._4._5
 
         private void DrawFingerprints()
         {
-            if (true)
+            using (IAudio proxy = new BassProxy())
             {
-                SaveFileDialog sfd = new SaveFileDialog
+                Fingerprinter manager = new Fingerprinter();
+                // Not 100% sure what stridenumber means
+                int strideSize = 1102;
+                int samplesPerFingerprint = 128 * 64; // 128 = width of fingerprint, 64 = overlap
+                var stride = new IncrementalStaticStride(strideSize, samplesPerFingerprint);
+
+                int totalFingerprints = 0;
+
+                //List<bool[]> fingerprints = manager.CreateFingerprints(proxy, Path.GetFullPath(filename), stride);
+                List<Fingerprint> fingerprints = manager.CreateFingerprints(proxy, Path.GetFullPath(filename), stride);
+                var test = manager.GetFingerHashes(stride, fingerprints, null);
+                //Console.WriteLine("Preliminary: " + preliminaryFingerprints.Count + " ---- " + test[1].HashBins[1]);
+                foreach (var fingerprint in test)
                 {
-                    Filter = "(*.jpg)|*.jpg",
-                    FileName = Path.GetFileNameWithoutExtension(filename) + "_fingerprint_" + ".jpg"
-                };
-
-                sfd.ShowDialog();
-                string path = Path.GetFullPath(sfd.FileName);
-                using (IAudio proxy = new BassProxy())
-                {
-                    Fingerprinter manager = new Fingerprinter();
-                    // Not 100% sure what stridenumber means
-                    int strideSize = 1102;
-                    int samplesPerFingerprint = 128*64; // 128 = width of fingerprint, 64 = overlap
-                    var stride = new IncrementalStaticStride(strideSize, samplesPerFingerprint);
-
-                    int totalFingerprints = 0;
-
-                    //List<bool[]> fingerprints = manager.CreateFingerprints(proxy, Path.GetFullPath(filename), stride);
-                    List<Fingerprint> fingerprints = manager.CreateFingerprints(proxy, Path.GetFullPath(filename), stride);
-                    //manager.GetHashSimilarity(stride, stride, proxy, filename, "C:\\Users\\Kristian\\Desktop\\c# musikkfile\\Pokemon_BlueRed_-_Route_1.wav");
-                    var test = manager.GetFingerHashes(stride, fingerprints, path);
-                    int width = manager.FingerprintLength;
-                    int height = manager.LogBins;
-                    Bitmap image = Imaging.GetFingerprintsImage(fingerprints, width, height);
-                    image.Save(path);
-                    image.Dispose();
+                    Console.WriteLine("SEQUENCE NO: " + fingerprint.SequenceNumber + " --- TIMESTAMP:" + fingerprint.Timestamp);
+                    for (int i = 0; i < fingerprint.HashBins.Length; i++)
+                    {
+                        Console.WriteLine("SUB FINGERPRINT: " + fingerprint.HashBins[i]);
+                    }  
                 }
+                Console.WriteLine("Done");
+                //manager.GetHashSimilarity(stride, stride, proxy, filename, "C:\\Users\\Kristian\\Desktop\\c# musikkfile\\Pokemon_BlueRed_-_Route_1.wav");
+                //var test = manager.GetFingerHashes(stride, fingerprints, path);
+                //int width = manager.FingerprintLength;
+                //int height = manager.LogBins;
+                //Bitmap image = Imaging.GetFingerprintsImage(fingerprints, width, height);
+                //image.Save(path);
+                //image.Dispose();
             }
-            // if each picture is in own file
-            /*
-            else
-            {
-                FolderBrowserDialog fbd = new FolderBrowserDialog();
-                if (fbd.ShowDialog() == DialogResult.OK)
-                {
-                    string path = fbd.SelectedPath;
-                    string fileName = Path.GetFileName(_tbPathToFile.Text);
-                    FadeControls(false);
-                    Action action = () =>
-                    {
-                        using (IAudio proxy = new BassProxy())
-                        {
-                            FingerprintManager manager = new FingerprintManager();
-                            StaticStride stride = new StaticStride((int)_nudStride.Value);
-                            List<bool[]> result = manager.CreateFingerprints(proxy, Path.GetFullPath(_tbPathToFile.Text), stride);
-                            int i = -1;
-                            int width = manager.FingerprintLength;
-                            int height = manager.LogBins;
-                            foreach (bool[] item in result)
-                            {
-                                Image image = Imaging.GetFingerprintImage(item, width, height);
-                                image.Save(path + "\\" + fileName + i++ + ".jpg", ImageFormat.Jpeg);
-                            }
-                        }
-                    };
-                    action.BeginInvoke((result) =>
-                    {
-                        FadeControls(true);
-                        MessageBox.Show(Resources.ImageIsDrawn, Resources.Finished, MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        action.EndInvoke(result);
-                    }
-                        , action);
-                }
-            }*/
         }
+    
 
         private string Selector(bool itemin)
         {
@@ -186,7 +133,7 @@ namespace DatabasePopulationApplication_0._4._5
                 image.Save(sfd.FileName, ImageFormat.Jpeg);
                 image.Dispose();
             }
-            //
+            
         }
 
         private void drawWavelets()
@@ -218,24 +165,6 @@ namespace DatabasePopulationApplication_0._4._5
             })).Start();
         }
 
-        private void SplitButton_Click(object sender, RoutedEventArgs e)
-        {
-            (new Thread(() =>
-            {
-                try
-                {
-                    Main.Status = "Not implemented.";
-                    //_splitVideosList = FingerprintManager.SplitWavFile(_preprocessedMedia, _preprocessedMedia);
-                }
-                catch (NullReferenceException ex)
-                {
-                    Console.WriteLine(ex);
-                    // Main.Status = "Choose an input file to preprocess first.";
-                }
-            })).Start();
-
-        }
-
 
         internal string Status
         {
@@ -260,10 +189,7 @@ namespace DatabasePopulationApplication_0._4._5
             {
                 Main.Status = "Purging database.";
             }
-            if (dialog.DialogResult == false)
-            {
-                Main.Status = "I am glad to see that you changed your mind.";
-            }
+
             //FingerprintManager.DeleteTable();
             //Main.Status = "Table has been deleted. Don't forget to create it again.";
         }
@@ -273,9 +199,7 @@ namespace DatabasePopulationApplication_0._4._5
         {
             (new Thread(() =>
             {
-
-                //FingerprintManager.CreateTable();
-                //Main.Status = "Table has been created.";
+                Main.Status = "Table has been created.";
             })).Start();
         }
 
@@ -307,8 +231,7 @@ namespace DatabasePopulationApplication_0._4._5
         private void hashButton_Click(object sender, RoutedEventArgs e)
         {
             DrawFingerprints();
-            //throw new NotImplementedException();
-            //FingerprintManager.HashTransform(_splitVideosList);
         }
     }
+
 }
